c1d9e29521eda2880241c718c70d790d1d264b75
griguk77 <griguk77@gmail.com>
2023-07-23 01:57:28 +0300

    Adding fragments and their layouts

---
+++ app/src/main/java/ru/studyguk/composition/presentation/WelcomeFragment.kt
@@ -0,0 +1,19 @@
+package ru.studyguk.composition.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.studyguk.composition.R
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+}
