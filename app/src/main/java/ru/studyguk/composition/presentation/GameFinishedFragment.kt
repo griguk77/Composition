503f89d9a5b083779cc8cedc6c8bbacfeed02b5d
griguk77 <griguk77@gmail.com>
2023-07-26 20:30:45 +0300

    Finished base version of project

--- app/src/main/java/ru/studyguk/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/ru/studyguk/composition/presentation/GameFinishedFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import ru.studyguk.composition.R
 import ru.studyguk.composition.databinding.FragmentGameFinishedBinding
 import ru.studyguk.composition.domain.entity.GameResult
 
@@ -32,7 +33,10 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun retryGame() {
-        requireActivity().supportFragmentManager.popBackStack(GameFragment.NAME, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        requireActivity().supportFragmentManager.popBackStack(
+            GameFragment.NAME,
+            FragmentManager.POP_BACK_STACK_INCLUSIVE
+        )
     }
 
     override fun onCreateView(
@@ -55,6 +59,38 @@ class GameFinishedFragment : Fragment() {
         )
         binding.buttonRetry.setOnClickListener {
             retryGame()
+        }
+        bindViews()
+    }
+
+    private fun bindViews() {
+        with(binding) {
+            emojiResult.setImageResource(getSmileResId())
+            tvRequiredAnswers.text = String.format(
+                resources.getString(R.string.requireRightAnswersText),
+                gameResult.gameSettings.minCountOfRightAnswers
+            )
+            tvScoreAnswers.text = String.format(
+                resources.getString(R.string.scoreResultText),
+                gameResult.countOfRightAnswers
+            )
+            tvRequiredPercentage.text = String.format(
+                resources.getString(R.string.requireRightAnswersPercentageText),
+                gameResult.gameSettings.minPercentOfRightAnswers
+            )
+            tvScorePercentage.text = String.format(
+                resources.getString(R.string.scoreRightAnswersPercentage),
+                (((gameResult.countOfRightAnswers / gameResult.countOfQuestions.toDouble()) * 100).toInt()).toString()
+            )
+        }
+    }
+
+
+    private fun getSmileResId(): Int {
+        return if (gameResult.winner) {
+            R.drawable.ic_smile
+        } else {
+            R.drawable.ic_sad
         }
     }
 
